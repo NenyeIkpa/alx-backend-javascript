c728a6f6b90b5f18073b7fc7e30ae1cf5d50bb0f
NenyeIkpa <nenyeikpa@gmail.com>
2024-04-12 11:10:25 +0000

    task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Constructor = Object.getPrototypeOf(this).constructor;
+    return new Constructor();
+  }
+}
+export default Car;
