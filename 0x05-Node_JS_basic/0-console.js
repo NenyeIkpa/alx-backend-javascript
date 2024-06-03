01c2375b12bd14a206e80a0abf5b4518987d945f
NenyeIkpa <nenyeikpa@gmail.com>
2024-06-03 09:45:04 +0000

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * displays a msg on stdout
+ * @param {String} msg to be displayed
+ * @author Chinenye Ikpa <https://github.com/NenyeIkpa>
+ */
+
+const displayMessage= (msg) => {
+  console.log(msg);
+}
+
+module.exports = displayMessage;
